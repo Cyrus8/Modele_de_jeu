c0b45bba276a24409997c71e582769a1536a3419
Thomas Moras <moras.thomas@gmail.com>
2015-03-26 17:07:32 +0100

    Revert "Revert "4""
    This reverts commit dcb34bcefcd13e4fee51524939f379e7008683b3.

---
+++ src/portalQuest/TimerThread.java
@@ -0,0 +1,13 @@
+package portalQuest;
+//TimerThread.java
+ 
+public class TimerThread extends Thread{
+        public static int MILLI = 0;
+        public void run(){
+                while(true){
+                        try {Thread.sleep(1);}
+                        catch (InterruptedException e) {e.printStackTrace();}
+                        MILLI++;
+                }
+        }
+}
